789c6e8576b21944420868d913fe235a4b301128
soo-na <soo.soojung.na@gmail.com>
2019-11-08 15:58:42 -0500

    change class component <BullsCows/> to hooks

--- BullsCows/bullsCows.jsx
+++ BullsCows/bullsCows.jsx
@@ -1,25 +1,24 @@
 const React = require('react');
+const {useState, useRef} = React;
 const UserTry = require('./userTry');
 
-class BullsCows extends React.Component{
-    state={
-        result:'',
-        value:'',
-        answer: getValue(),
-        tries:[],
-        over:false,
-    }
-    
-    onSubmit=(e)=>{
+//change to react hooks 
+const BullsCows = ()=>{
+
+    const [result, setResult] = useState('');
+    const [value, setValue] = useState('');
+    const [answer, setAnswer] = useState(getValue());
+    const [tries, setTries] = useState([]);
+    const [over, setOver] = useState(false);
+    const inputRef= useRef('reff');
+
+    const onSubmit=(e)=>{
         e.preventDefault();
         
-        if(!this.state.over){
-        const answer = this.state.answer;
-        const value  = this.state.value;
-        
+        if(!over){
         //1. check input length is 4
         if(value.length!==4){ 
-            this.setState({result:'Enter "four" digits!'}) 
+            setResult('Enter "four" digits!');
             return; 
         }
         
@@ -34,7 +33,7 @@ class BullsCows extends React.Component{
        //2. check if unique characters
         for(let [k,v] of Object.entries(hs)){
             if(v>1){
-                this.setState({result:'Enter "unique" four digits!'});
+                setResult('Enter "unique" four digits!');
                 return;
             }
         }
@@ -59,106 +58,68 @@ class BullsCows extends React.Component{
         t.ball = ball;
         t.strike = strike;
 
-        this.setState((prevState)=>{
-            return{
-                tries: [...prevState.tries, t],
-                value:'',
-            }});
+        setTries([...tries, t]);/////
+        setValue('');
+       
 
-        if(strike===4){      
-        this.setState(()=>{ 
-            return{
-                result: this.state.answer.join('') + ' is answer. Congratulations! reset game.',
-                over:true,
-        }})}else{
-            this.setState({result: `you have ${10 - this.state.tries.length -1} more tries!` });
+        if(strike===4){ 
+            setResult(answer.join('') + ' is answer. Congratulations! reset game.');
+            setOver(true);
+        }else{
+            setResult(`you have ${10 - tries.length -1} more tries!` );
         }
        
-        if(this.state.tries.length>=9){
-            this.setState(()=>{
-                return{
-                    over: true,
-                    result:'Woops! game over. Anser is '+this.state.answer.join('') +' reset to try again.'
-                }
-            });
+        if(tries.length>=9){
+            setOver(true);
+            setResult('Woops! game over. Answer is '+answer.join('') +' reset to try again.');
+        };
         }
-
-        }
-    this.input.focus();
-       return;
+        inputRef.current.focus();//
+         return;
     }
 
-    onRefInput=(c)=>{
-        this.input=c;
+   const onChange=(e)=>{
+        setValue(e.target.value);
     }
-
-    onChange=(e)=>{
     
-        this.setState({value: e.target.value})
-    }
-
-    input;
-
-    onReset=(e)=>{
-        this.setState(()=>{ 
-            return{
-                value:'',
-                tries:[],
-                answer:getValue(),
-                result:'',
-                over:false,
-            }
-        } )
-    }
-
-
-
-
-    render(){
-
-
-        const t = this.state.tries.map((t,i)=>{
-            return <div key={'try'+i}>
-                <li><b>{t.value}</b> :  {t.ball} ball(s)  | {t.strike} strike(s)</li>
-
-            </div>
-        })
-
+    const onReset=(e)=>{
+        setValue('');
+        setTries([]);
+        setAnswer(getValue());
+        setResult('');
+        setOver(false);
         
-
-
-
-        return(
-            <>
-           <h2>Enter 4 different digits between 0 ~ 9, then click "ok"</h2>
-           
-            <form onSubmit={this.onSubmit}>    
-                
-                <input type="number"  value={this.state.value} onChange={this.onChange}
-                 ref={this.onRefInput}/>
-                    <button>ok</button>
-                   <h3>{this.state.result}</h3>
-            </form>
-           
-            <div><UserTry value={t}></UserTry> </div>
-               <button onClick={this.onReset}>reset</button>
-
-        </>
-
-    )
-    }
     }
 
+    const t = tries.map((t,i)=>{
+        return (
+           <div key={'try'+i}>
+             <li><b>{t.value}</b> :  {t.ball} ball(s)  | {t.strike} strike(s)</li>
+           </div>
+        )
+    })
+    return(<>
+
+
+       <h2>Enter 4 different digits between 0 ~ 9, then click "ok"</h2>
+           <form onSubmit={onSubmit}>    
+               <input type="number"  value={value} onChange={onChange} ref={inputRef}/>
+                   <button>ok</button>
+                  <h3>{result}</h3>
+           </form>
+           <div><UserTry value={t}></UserTry> </div>
+        <button onClick={onReset}>reset</button>
+    </>);
+}
 const getValue =()=>{
     var arr = [];
     for(var i =0; arr.length<4 ; i++){
         const e= Math.ceil(Math.random()*9);
-        if(!arr.find((element)=>{ return element ===e})){
+        if(!arr.find((n)=>{ return n ===e})){
             arr.push(e);
         }
     }
-    return arr;
-    
+    return arr;  
 }
 
 module.exports = BullsCows;
