a3412e1f2f739c2bcaf8182af5565616d722bd5b
soo-na <soo.soojung.na@gmail.com>
2019-11-12 17:53:34 -0500

    add setTimeout()

--- RockPaperSiser/rockPaperSiser.jsx
+++ RockPaperSiser/rockPaperSiser.jsx
@@ -1,5 +1,6 @@
 const  React =  require('react');
 const {PureComponent}= React;
+
 const rpsCoords ={
     rock:0,
     siser:'-142px',
@@ -14,6 +15,7 @@ const computerChoice=(imgCoord)=>{
         return Object.entries(rpsCoords).find((rpsCoord)=>
         rpsCoord[1] ===imgCoord)[0];
 }
+
 class RPS extends PureComponent{
     state={
         result:'',
@@ -22,18 +24,13 @@ class RPS extends PureComponent{
     }
 
     interval;
-
-
     changeHand=()=>{
-            
         const {imgCoord} = this.state;
         if(imgCoord === rpsCoords.siser){
 
             this.setState({imgCoord:rpsCoords.rock});
 
         }else if(imgCoord === rpsCoords.paper){
-
-
             this.setState({imgCoord:rpsCoords.siser});
         }
         else if(imgCoord=== rpsCoords.rock){
@@ -42,8 +39,7 @@ class RPS extends PureComponent{
         }
     }
     componentDidMount(){//call setInterval asyn request only when it render first
-        
-        this.interval = setInterval(this.changeHand ,50)
+        this.interval = setInterval(this.changeHand ,50);
     }
 
     componentWillUnmount(){
@@ -51,12 +47,11 @@ class RPS extends PureComponent{
 
     }
 
-    onClickBtn=(choice)=>()=>{
-
-        const  {imgCoord} =this.state;
+    onClickBtn=(choice)=>{
 
         clearInterval(this.interval);
-        
+        const  {imgCoord} =this.state;
+
         //siser:1, paper:-1, rock:0
         // lose: siser-rock//1, paper-siser//-2, rock-paper//1
         // win: siser-paper//2, paper-rock//-1, rock-siser//-1
@@ -70,7 +65,7 @@ class RPS extends PureComponent{
             })
         }else if([-1,2].includes(diff)){
                 this.setState((prevState)=>{return{
-                    result:'you won!',
+                    result:'you win!',
                     score: prevState.score +1,
                 } });
 
@@ -82,7 +77,7 @@ class RPS extends PureComponent{
 
         }
 
-        this.interval = setInterval(this.changeHand, 50)
+         setTimeout(()=>{ this.interval = setInterval(this.changeHand, 50)}, 1000);
 
     }
     render(){
@@ -92,10 +87,11 @@ class RPS extends PureComponent{
         return(<>
         <div id='computer' style={{background:`url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0px`}}>
         </div>
+
         <div>
-        <button id='rock' className='rpsBtn' onClick={this.onClickBtn('rock')}> rock</button>
-        <button id='siser' className='rpsBtn' onClick={this.onClickBtn('siser')}> siser</button>
-        <button id='paper' className='rpsBtn' onClick={this.onClickBtn('paper')}> paper</button>
+            <button id='rock' className='rpsBtn' onClick={()=>this.onClickBtn('rock')}> rock</button>
+            <button id='siser' className='rpsBtn' onClick={()=>this.onClickBtn('siser')}> siser</button>
+            <button id='paper' className='rpsBtn' onClick={()=>this.onClickBtn('paper')}> paper</button>
         </div>
            <div>{result}</div> 
            <div>score: {score}</div>
